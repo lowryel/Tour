734ea129e6de3f5b7a3d8adefa7845d2013a318e
[lowryel] <[eugeneagbaglo@gmail.com]>
2021-10-20 11:30:42 +0000

    [this is the latest msg]

--- tourapp/models.py
+++ tourapp/models.py
@@ -4,6 +4,7 @@ from django.db import models
 
 # Create your models here.
 
+
 class tourModel(models.Model):
     title = models.CharField(max_length=100, blank=False)
     date = models.DateField(auto_now_add=False)
@@ -13,17 +14,11 @@ class tourModel(models.Model):
     def __str__(self):
         return self.title
 
+
 class feedbackModel(models.Model):
     name = models.CharField(max_length=50, blank=False)
     tweethandl = models.CharField(max_length=50)
-    image = models.ImageField(upload_to="images/pics")
     tweet = models.TextField()
 
     def __str__(self):
         return self.name
-
-
-
-
-
-
--- tourapp/views.py
+++ tourapp/views.py
@@ -1,14 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import tourModel
 from .models import feedbackModel
+from .forms import feedbackForm
 
 # Create your views here.
+
+
 def tourism(request):
     tour_sites = tourModel.objects.all()
     feedback = feedbackModel.objects.all()
+
+    if request.method == "POST":
+        form = feedbackForm(request.POST)
+        if form.is_valid():
+            obj = form.save(commit=False)
+            obj.save()
+            return redirect('tourism')
+    else:
+        form = feedbackForm()
     context = {
         "sites": tour_sites,
         "feeds": feedback,
+        "form": form
     }
     return render(request, 'tourism.html', context)
-
