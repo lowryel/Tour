d157df6b2959ef58c0ed050ee1b9fe4631e62129
lowryel <eugeneagbaglo@gmail.com>
2021-12-18 14:27:54 +0000

    rectified a merge conflict

--- tourapp/forms.py
+++ tourapp/forms.py
@@ -7,12 +7,3 @@ class feedbackForm(ModelForm):
     class Meta:
         model = feedbackModel
         fields = ['name', 'tweet', 'tweethandl']
-from django.forms import ModelForm
-
-from .models import feedbackModel
-
-
-class feedbackForm(ModelForm):
-    class Meta:
-        model = feedbackModel
-        fields = ['name', 'tweet', 'tweethandl']
